711613b83ad5d571c4a40075f23fea5f37a7400a
Erangamadhushan <erangamadhushann956@gmail.com>
2025-05-03 03:51:57 +0530

    Test TripPlannerController.java file functionality and Properties

--- BlueLanka_Guide/src/main/java/com/bluelanka_guide/controller/TripPlannerController.java
+++ BlueLanka_Guide/src/main/java/com/bluelanka_guide/controller/TripPlannerController.java
@@ -1,5 +1,21 @@
 package com.bluelanka_guide.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
 public class TripPlannerController {
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private PasswordField passwordTextField;
+
+    @FXML
+    protected void onTestButton() {
+        nameTextField.setDisable(true);
+        passwordTextField.setDisable(true);
+    }
 }
